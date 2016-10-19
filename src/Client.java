7699b39905dbef58df5abc484d3977f1fa2deb0a
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-10-19 17:12:50 +0100

    First commit

---
+++ src/Client.java
@@ -0,0 +1,30 @@
+public class Client {
+
+    private boolean pwd() {
+        // Prints the current directory
+        return false;
+    }
+
+    private boolean ls() {
+        // Lists the current directory
+        return false;
+    }
+
+    private boolean cd(String dir) {
+        // Changes the current directory to dir
+        // dir can be a simple name or absolute or relative path
+        return false;
+    }
+
+    private boolean mv(String file1, String file2) {
+        // Copies file file1 to file2, overwriting if the latter exists
+        // file can be a simple name or absolute or relative path
+        return false;
+    }
+
+    private boolean open(String file) {
+        // Opens the file with the proper application, accordingly to its extension
+        // file can be a simple name or absolute or relative path
+        return false;
+    }
+}
