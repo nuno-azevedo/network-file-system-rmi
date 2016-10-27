9ebab4f10b91fb31697dec561b12e77185b51554
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-10-28 00:08:54 +0100

    Methods init, close, create and delete
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/StorageServer.java
+++ src/StorageServer.java
@@ -1,26 +1,31 @@
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 interface StorageInterface extends Remote {
     // CALLS FROM CLIENT
-    public boolean init(String local_path, String filesystem_path) throws RemoteException;
+    boolean init(String local_path, String filesystem_path) throws RemoteException;
 
-    public boolean close(String local_path) throws RemoteException;
+    boolean close(String local_path) throws RemoteException;
 
-    public boolean create(String path) throws RemoteException;
+    boolean create(String path) throws RemoteException;
 
-    public boolean create(String path, String blob) throws RemoteException;
+    boolean create(String path, String blob) throws RemoteException;
 
-    public boolean del(String path) throws RemoteException;
+    boolean del(String path) throws RemoteException;
 
-    public boolean get(String path) throws RemoteException;
+    boolean get(String path) throws RemoteException;
 }
 
 public class StorageServer implements StorageInterface {
+    HashMap<String, String> users = new HashMap<String, String>();
+
     private StorageServer() {
         // Class constructor
     }
@@ -33,8 +38,6 @@ public class StorageServer implements StorageInterface {
             // Bind the remote object's stub in the registry
             Registry registry = LocateRegistry.getRegistry();
             registry.bind("StorageServer", stub);
-
-            System.err.println("Server ready");
         } catch (Exception e) {
             System.err.println(e.toString());
             e.printStackTrace();
@@ -45,39 +48,51 @@ public class StorageServer implements StorageInterface {
     public boolean init(String local_path, String filesystem_path) {  // On startup
         // Example: init("/home/student/courses", "/courses"); -> Local dir maps into global namespace
         //                                                        Must call add_storage_server on the metadata server
-        String path = "storage" + (filesystem_path.startsWith("/") ? filesystem_path : "/" + filesystem_path);
-        boolean success = (new File(path)).mkdirs();
-        if (!success) {
-            System.err.println("Failed to create filesystem dir.");
-            return false;
-        }
-        return true;
+        users.put(cleanPath(local_path), cleanPath(filesystem_path));
+        return create(filesystem_path);
     }
 
     public boolean close(String local_path) {  // On close
         // Example: close("/home/student/courses"); -> Closes local share
         //                                             Must call del_storage_server on the metadata server
-        return false;
+        String file_system_path = users.get(cleanPath(local_path));
+        users.remove(cleanPath(local_path));
+        return del(file_system_path);
     }
 
     public boolean create(String path) {
         // Example: create("/courses"); -> Creates a directory
-        return false;
+        return new File(cleanPath(path)).mkdirs();
     }
 
     public boolean create(String path, String blob) {
         // Example: create("/courses/file1.txt", ”A line in a text”); -> Creates a file
-        return false;
+        try {
+            Files.write(Paths.get(cleanPath(path)), blob.getBytes());
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+        }
+        return true;
     }
 
     public boolean del(String path) {
         // Example: del("/courses"); -> Removes sub-tree
         // Example: del("/courses/file1.txt"); -> Removes file
-        return false;
+        return delete(new File(cleanPath(path)));
+    }
+
+    private boolean delete(File path) {
+        if (path.isDirectory())
+            for (File p : path.listFiles())
+                delete(p);
+        return path.delete();
     }
 
     public boolean get(String path) {
         // Example: get("/courses/file1.txt"); -> Downloads the file
-        return false;
+        return true;
     }
+
+    private String cleanPath(String path) { return path.replaceAll("^/", "").replaceAll("/$", ""); }
 }
