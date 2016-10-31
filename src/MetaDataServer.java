2fc2166b12960c3ac238eca446169ee7b5e9f82d
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-10-31 00:26:06 +0000

    MetaDataInterface and RMI Registry
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/MetaDataServer.java
+++ src/MetaDataServer.java
@@ -1,51 +1,82 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 
-public class MetaDataServer {
-    private static FSTree fileSystem = new FSTree();
-    private static HashMap<String, String> storageServers = new HashMap<String, String>();
+interface MetaDataInterface extends Remote {
+    // CALLS FROM STORAGE SERVER
+    boolean addStorageServer(String host, String top_dir) throws RemoteException;
+
+    boolean delStorageServer(String top_dir) throws RemoteException;
+
+    boolean addStorageItem(String item) throws RemoteException;
+
+    boolean delStorageItem(String item) throws RemoteException;
+
+    // CALLS FROM CLIENT
+    String find(String path) throws RemoteException;
+
+    Stat lstat(String path) throws RemoteException;
+}
+
+public class MetaDataServer implements MetaDataInterface {
+    private static FSTree fileSystem;
+    private static HashMap<String, String> storageServers;
 
     private MetaDataServer() {
-        // Class constructor
+        this.fileSystem = new FSTree();
+        this.storageServers = new HashMap<String, String>();
     }
 
     public static void main(String args[]) {
-        addStorageServer("dcc.fc.up.pt", "dcc");
-        addStorageItem("/dcc/sd/aulas");
-        addStorageItem("/dcc/cp/aulas");
-        System.out.println(lstat("/dcc/").toString());
+        try {
+            MetaDataServer obj = new MetaDataServer();
+            MetaDataInterface stub = (MetaDataInterface) UnicastRemoteObject.exportObject(obj, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("MetaDataServer", stub);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+        }
     }
 
     // CALLS FROM STORAGE SERVER
-    public static void addStorageServer(String host, String top_dir) {
+    public boolean addStorageServer(String host, String top_dir) {
         // Example: addStorageServer("machine1.dcc.fc.up.pt", "/courses");
         storageServers.put(top_dir, host);
         fileSystem.addNode(top_dir);
+        return true;
     }
 
-    public static void delStorageServer(String top_dir) {
+    public boolean delStorageServer(String top_dir) {
         // Example: delStorageServer("/courses");
         storageServers.remove(top_dir);
         fileSystem.delNode(top_dir);
+        return true;
     }
 
-    public static void addStorageItem(String item) {
+    public boolean addStorageItem(String item) {
         // Example: addStorageItem("/courses/video1.avi");
         fileSystem.addNode(item);
+        return true;
     }
 
-    public static void delStorageItem(String item) {
+    public boolean delStorageItem(String item) {
         // Example: delStorageItem("/courses/video1.avi");
         fileSystem.delNode(item);
+        return true;
     }
 
     // CALLS FROM CLIENT
-    public static String find(String path) {
+    public String find(String path) {
         // Example: find("/courses"); -> "machine1.dcc.fc.up.pt"
         String top_dir = splitPath(path)[0];
         return storageServers.get(top_dir);
     }
 
-    public static Stat lstat(String path) {
+    public Stat lstat(String path) {
         // Example: lstat("/courses"); -> { "machine1.dcc.fc.up.pt", { "afile.txt", "bfile.txt", "..." } }
         Stat stat = new Stat();
         stat.server = find(path);
@@ -54,7 +85,7 @@ public class MetaDataServer {
         return stat;
     }
 
-    private static String[] splitPath(String path) {
+    private String[] splitPath(String path) {
         return path.replaceAll("^/", "").replaceAll("/$", "").split("/");
     }
 }
