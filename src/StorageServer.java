c323635cf8a6803dc79bbe87f10c11d640ff6540
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-11-01 16:04:06 +0000

    Check errors and exceptions
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/StorageServer.java
+++ src/StorageServer.java
@@ -1,112 +1,231 @@
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.FileWriter;
 import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 interface StorageInterface extends Remote {
     // CALLS FROM CLIENT
-    boolean init(String local_path, String filesystem_path) throws RemoteException, UnknownHostException;
+    void init(String local_path, String filesystem_path) throws Exception;
 
-    boolean close(String local_path) throws RemoteException;
+    void close(String local_path) throws Exception;
 
-    boolean create(String path) throws RemoteException;
+    void create(String path) throws Exception;
 
-    boolean create(String path, String blob) throws IOException;
+    void create(String path, String blob) throws Exception;
 
-    boolean del(String path) throws RemoteException;
+    void del(String path) throws Exception;
 
-    File get(String path) throws RemoteException;
+    File get(String path) throws Exception;
 }
 
 public class StorageServer implements StorageInterface {
-    private static MetaDataInterface metaData;
-    private HashMap<String, String> users;
+    private static final Logger Log = Logger.getLogger(StorageServer.class.getName());
+    private static String hostname;
+
+    private static MetaDataInterface MetaData;
+    private HashMap<String, String> Users;
 
     private StorageServer() {
-        this.users = new HashMap<String, String>();
+        this.Users = new HashMap<String, String>();
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
+        try {
+            hostname = args[0];
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "Missing hostname argument");
+            return;
+        }
+
         try {
             StorageServer obj = new StorageServer();
             StorageInterface stub = (StorageInterface) UnicastRemoteObject.exportObject(obj, 0);
             Registry registry = LocateRegistry.getRegistry();
-            registry.bind("StorageServer", stub);
+            registry.bind(hostname, stub);
 
-            metaData = (MetaDataInterface) registry.lookup("MetaDataServer");
+            MetaData = (MetaDataInterface) registry.lookup("MetaData");
         } catch (Exception e) {
-            System.err.println(e.toString());
+            Log.log(Level.SEVERE, "main: ‘" + e.toString() + "’", e);
             e.printStackTrace();
         }
     }
 
     // CALLS FROM CLIENT
-    public boolean init(String local_path, String filesystem_path) throws UnknownHostException, RemoteException {
+    public void init(String local_path, String filesystem_path) throws Exception {
         // On startup
         // Example: init("/home/student/courses", "/courses"); -> Local dir maps into global namespace
         //                                                        Must call add_storage_server on the metadata server
-        String clean_local_path = cleanPath(local_path);
-        String clean_filesystem_path = cleanPath(filesystem_path);
-        String hostname = InetAddress.getLocalHost().getHostName();
-        users.put(clean_local_path, clean_filesystem_path);
-        metaData.addStorageServer(hostname, clean_filesystem_path);
-        return create(clean_filesystem_path);
+        if (!checkPath(local_path)) {
+            Log.log(Level.SEVERE, "init: invalid local path", local_path);
+            throw new Exception("invalid local path ‘" + local_path + "’");
+        }
+        if (!checkTopDir(filesystem_path)) {
+            Log.log(Level.SEVERE, "init: invalid file system path", filesystem_path);
+            throw new Exception("invalid file system path ‘" + filesystem_path + "’");
+        }
+
+        try {
+            MetaData.addStorageServer(hostname, filesystem_path);
+            Users.put(local_path, filesystem_path);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "init: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+
+        File top_dir = new File("." + filesystem_path);
+        if (!top_dir.mkdir()) {
+            Log.log(Level.SEVERE, "init: could not create directory", filesystem_path);
+            throw new Exception("could not create directory ‘" + filesystem_path + "’");
+        }
+        Log.log(Level.INFO, "init: ‘" + filesystem_path + "’", local_path);
     }
 
-    public boolean close(String local_path) throws RemoteException {
+    public void close(String local_path) throws Exception {
         // On close
         // Example: close("/home/student/courses"); -> Closes local share
         //                                             Must call del_storage_server on the metadata server
-        String clean_local_path = cleanPath(local_path);
-        String file_system_path = users.get(clean_local_path);
-        metaData.delStorageServer(file_system_path);
-        users.remove(clean_local_path);
-        return del(file_system_path);
+        if (!checkPath(local_path)) {
+            Log.log(Level.SEVERE, "close: invalid local path", local_path);
+            throw new Exception("invalid local path ‘" + local_path + "’");
+        }
+
+        String filesystem_path = Users.get(local_path);
+        if (filesystem_path == null) {
+            Log.log(Level.SEVERE, "close: local path not found", local_path);
+            throw new Exception("local path ‘" + local_path + "’ not found");
+        }
+
+        try {
+            MetaData.delStorageServer(filesystem_path);
+            Users.remove(local_path);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "close: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+
+        File top_dir = new File("." + filesystem_path);
+        if (!delRecursive(top_dir)) {
+            Log.log(Level.SEVERE, "close: could not delete directory", filesystem_path);
+            throw new Exception("could not delete directory ‘" + filesystem_path + "’");
+        }
+        Log.log(Level.INFO, "close: ‘" + filesystem_path + "’", local_path);
     }
 
-    public boolean create(String path) throws RemoteException {
+    public void create(String path) throws Exception {
         // Example: create("/courses"); -> Creates a directory
-        String clean_path = cleanPath(path);
-        metaData.addStorageItem(clean_path);
-        return new File(clean_path).mkdirs();
+        if (!checkPath(path)) {
+            Log.log(Level.SEVERE, "create: invalid path", path);
+            throw new Exception("invalid path ‘" + path + "’");
+        }
+
+        try {
+            MetaData.addStorageItem(path);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "create: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+
+        File dir = new File("." + path);
+        if (!dir.mkdir()) {
+            Log.log(Level.SEVERE, "create: could not create directory", path);
+            throw new Exception("could not create directory ‘" + path + "’");
+        }
+        Log.log(Level.INFO, "create: ‘" + path + "’");
     }
 
-    public boolean create(String path, String blob) throws IOException {
+    public void create(String path, String blob) throws Exception {
         // Example: create("/courses/file1.txt", ”A line in a text”); -> Creates a file
-        String clean_path = cleanPath(path);
-        metaData.addStorageItem(clean_path);
-        Files.write(Paths.get(clean_path), blob.getBytes());
-        return true;
+        if (!checkPath(path)) {
+            Log.log(Level.SEVERE, "create: invalid path", path);
+            throw new Exception("invalid path ‘" + path + "’");
+        }
+
+        try {
+            MetaData.addStorageItem(path);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "create: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+
+        File file = new File("." + path);
+        if (!file.createNewFile()) {
+            Log.log(Level.SEVERE, "create: could not create file", path);
+            throw new Exception("could not create file ‘" + path + "’");
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(blob);
+            writer.close();
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "create: could not write to file", path);
+            throw new Exception("could not write to file ‘" + path + "’");
+        }
+        Log.log(Level.INFO, "create: ‘" + path + "’");
     }
 
-    public boolean del(String path) throws RemoteException {
+    public void del(String path) throws Exception {
         // Example: del("/courses"); -> Removes sub-tree
         // Example: del("/courses/file1.txt"); -> Removes file
-        String clean_path = cleanPath(path);
-        metaData.delStorageItem(clean_path);
-        return delete(new File(clean_path));
+        if (!checkPath(path)) {
+            Log.log(Level.SEVERE, "del: invalid path", path);
+            throw new Exception("invalid path ‘" + path + "’");
+        }
+
+        try {
+            MetaData.delStorageItem(path);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "del: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+        File target = new File("." + path);
+        if (!target.exists()) {
+            Log.log(Level.SEVERE, "del: no such file or directory", path);
+            throw new Exception("no such file or directory ‘" + path + "’");
+        }
+        if (!delRecursive(target)) {
+            Log.log(Level.SEVERE, "del: could not delete file or directory", path);
+            throw new Exception("could not delete file or directory ‘" + path + "’");
+        }
+        Log.log(Level.INFO, "del: ‘" + path + "’");
     }
 
-    private boolean delete(File f) {
+    private boolean delRecursive(File f) {
         if (f.isDirectory())
             for (File c : f.listFiles())
-                delete(c);
+                delRecursive(c);
         return f.delete();
     }
 
-    public File get(String path) {
+    public File get(String path) throws Exception {
         // Example: get("/courses/file1.txt"); -> Downloads the file
-        String clean_path = cleanPath(path);
-        return new File(clean_path);
+        if (!checkPath(path)) {
+            Log.log(Level.SEVERE, "get: invalid path", path);
+            throw new Exception("invalid path ‘" + path + "’");
+        }
+
+        File target = new File("." + path);
+        if (!target.exists()) {
+            Log.log(Level.SEVERE, "get: no such file or directory", path);
+            throw new Exception("no such file or directory ‘" + path + "’");
+        }
+        return target;
     }
 
-    private String cleanPath(String path) { return path.replaceAll("^/", "").replaceAll("/$", ""); }
+    private boolean checkTopDir(String top_dir) {
+        String valid_top_dir = "^/[^/]*/?$";
+        if (top_dir.matches(valid_top_dir)) return true;
+        return false;
+    }
+
+    private boolean checkPath(String path) {
+        String valid_path = "^/([^/]+/?)+$";
+        if (path.matches(valid_path)) return true;
+        return false;
+    }
 }
