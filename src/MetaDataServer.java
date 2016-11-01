749a730fe61a5c24bdc261fe087ea5b76e4efb52
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-11-01 16:03:31 +0000

    Check errors and exceptions
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/MetaDataServer.java
+++ src/MetaDataServer.java
@@ -1,33 +1,36 @@
 import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 interface MetaDataInterface extends Remote {
     // CALLS FROM STORAGE SERVER
-    boolean addStorageServer(String host, String top_dir) throws RemoteException;
+    void addStorageServer(String host, String top_dir) throws Exception;
 
-    boolean delStorageServer(String top_dir) throws RemoteException;
+    void delStorageServer(String top_dir) throws Exception;
 
-    boolean addStorageItem(String item) throws RemoteException;
+    void addStorageItem(String item) throws Exception;
 
-    boolean delStorageItem(String item) throws RemoteException;
+    void delStorageItem(String item) throws Exception;
 
     // CALLS FROM CLIENT
-    String find(String path) throws RemoteException;
+    String find(String path) throws Exception;
 
-    Stat lstat(String path) throws RemoteException;
+    Stat lstat(String path) throws Exception;
 }
 
 public class MetaDataServer implements MetaDataInterface {
-    private static FSTree fileSystem;
-    private static HashMap<String, String> storageServers;
+    private static final Logger Log = Logger.getLogger(MetaDataServer.class.getName());
+
+    private static FSTree FileSystem;
+    private static HashMap<String, String> StorageServers;
 
     private MetaDataServer() {
-        this.fileSystem = new FSTree();
-        this.storageServers = new HashMap<String, String>();
+        this.FileSystem = new FSTree();
+        this.StorageServers = new HashMap<String, String>();
     }
 
     public static void main(String args[]) {
@@ -35,54 +38,136 @@ public class MetaDataServer implements MetaDataInterface {
             MetaDataServer obj = new MetaDataServer();
             MetaDataInterface stub = (MetaDataInterface) UnicastRemoteObject.exportObject(obj, 0);
             Registry registry = LocateRegistry.getRegistry();
-            registry.bind("MetaDataServer", stub);
+            registry.bind("MetaData", stub);
         } catch (Exception e) {
-            System.err.println(e.toString());
+            Log.log(Level.SEVERE, "main: ‘" + e.toString() + "’", e);
             e.printStackTrace();
         }
     }
 
     // CALLS FROM STORAGE SERVER
-    public boolean addStorageServer(String host, String top_dir) {
+    public void addStorageServer(String host, String top_dir) throws Exception {
         // Example: addStorageServer("machine1.dcc.fc.up.pt", "/courses");
-        storageServers.put(top_dir, host);
-        fileSystem.addNode(top_dir);
-        return true;
+        if (!checkTopDir(top_dir)) {
+            Log.log(Level.SEVERE, "addStorageServer: invalid top directory", top_dir);
+            throw new Exception("invalid top directory ‘" + top_dir + "’");
+        }
+        if (StorageServers.containsKey(top_dir)) {
+            Log.log(Level.SEVERE, "addStorageServer: top directory already in use", top_dir);
+            throw new Exception("top directory ‘" + top_dir + "’already in use");
+        }
+
+        try {
+            FileSystem.addNode(top_dir);
+            StorageServers.put(top_dir, host);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "addStorageServer: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+        Log.log(Level.INFO, "addStorageServer: ‘" + host + "’", top_dir);
     }
 
-    public boolean delStorageServer(String top_dir) {
+    public void delStorageServer(String top_dir) throws Exception {
         // Example: delStorageServer("/courses");
-        storageServers.remove(top_dir);
-        fileSystem.delNode(top_dir);
-        return true;
+        if (!checkTopDir(top_dir)) {
+            Log.log(Level.SEVERE, "delStorageServer: invalid top directory", top_dir);
+            throw new Exception("invalid top directory ‘" + top_dir + "’");
+        }
+        if (!StorageServers.containsKey(top_dir)) {
+            Log.log(Level.SEVERE, "delStorageServer: top directory not found", top_dir);
+            throw new Exception("top directory ‘" + top_dir + "’not found");
+        }
+
+        try {
+            FileSystem.delNode(top_dir);
+            StorageServers.remove(top_dir);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "delStorageServer: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+        Log.log(Level.INFO, "delStorageServer: ‘" + top_dir + "’");
     }
 
-    public boolean addStorageItem(String item) {
+    public void addStorageItem(String item) throws Exception {
         // Example: addStorageItem("/courses/video1.avi");
-        fileSystem.addNode(item);
-        return true;
+        if (!checkPath(item)) {
+            Log.log(Level.SEVERE, "addStorageItem: invalid path", item);
+            throw new Exception("invalid path ‘" + item + "’");
+        }
+
+        try {
+            FileSystem.addNode(item);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "addStorageItem: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+        Log.log(Level.INFO, "addStorageItem: ‘" + item + "’");
     }
 
-    public boolean delStorageItem(String item) {
+    public void delStorageItem(String item) throws Exception {
         // Example: delStorageItem("/courses/video1.avi");
-        fileSystem.delNode(item);
-        return true;
+        if (!checkPath(item)) {
+            Log.log(Level.SEVERE, "delStorageItem: invalid path", item);
+            throw new Exception("invalid path ‘" + item + "’");
+        }
+
+        try {
+            FileSystem.delNode(item);
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "delStorageItem: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+        Log.log(Level.INFO, "delStorageItem: ‘" + item + "’");
     }
 
     // CALLS FROM CLIENT
-    public String find(String path) {
+    public String find(String path) throws Exception {
         // Example: find("/courses"); -> "machine1.dcc.fc.up.pt"
+        if (!checkPath(path)) {
+            Log.log(Level.SEVERE, "find: invalid path", path);
+            throw new Exception("invalid path ‘" + path + "’");
+        }
+
         String top_dir = splitPath(path)[0];
-        return storageServers.get(top_dir);
+        String host = StorageServers.get(top_dir);
+        if (host == null) {
+            Log.log(Level.SEVERE, "find: top directory not found", top_dir);
+            throw new Exception("top directory ‘" + top_dir + "’ not found");
+        }
+        Log.log(Level.INFO, "find: ‘" + path + "’", host);
+        return host;
     }
 
-    public Stat lstat(String path) {
+    public Stat lstat(String path) throws Exception {
         // Example: lstat("/courses"); -> { "machine1.dcc.fc.up.pt", { "afile.txt", "bfile.txt", "..." } }
+        if (!checkPath(path)) {
+            Log.log(Level.SEVERE, "lstat: invalid path", path);
+            throw new Exception("invalid path ‘" + path + "’");
+        }
+
         Stat stat = new Stat();
-        stat.server = find(path);
-        for (FSNode child : fileSystem.getNode(path).getChilds())
-            stat.items.add(child.getName());
+        try {
+            FSNode target = FileSystem.getNode(path);
+            stat.server = find(path);
+            stat.items = target.getChildsNames();
+        } catch (Exception e) {
+            Log.log(Level.SEVERE, "lstat: ‘" + e.toString() + "’", e);
+            throw e;
+        }
+        Log.log(Level.INFO, "lstat: ‘" + path + "’");
         return stat;
+    }
+
+    private boolean checkTopDir(String top_dir) {
+        String valid_top_dir = "^/[^/]*/?$";
+        if (top_dir.matches(valid_top_dir)) return true;
+        return false;
+    }
+
+    private boolean checkPath(String path) {
+        String valid_path = "^/([^/]+/?)+$";
+        if (path.matches(valid_path)) return true;
+        return false;
     }
 
     private String[] splitPath(String path) {
