87660a74cd6ff15b6461879739f008d498497205
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-11-04 04:05:30 +0000

    Logs improvement, several fixes
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/MetaDataServer.java
+++ src/MetaDataServer.java
@@ -2,13 +2,15 @@ import java.rmi.Remote;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 interface MetaDataInterface extends Remote {
     // CALLS FROM STORAGE SERVER
-    void addStorageServer(String host, String top_dir) throws Exception;
+    void addStorageServer(String hostname, String top_dir) throws Exception;
 
     void delStorageServer(String top_dir) throws Exception;
 
@@ -39,123 +41,140 @@ public class MetaDataServer implements MetaDataInterface {
             MetaDataInterface stub = (MetaDataInterface) UnicastRemoteObject.exportObject(obj, 0);
             Registry registry = LocateRegistry.getRegistry();
             registry.bind("MetaData", stub);
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        registry.unbind("MetaData");
+                    } catch (Exception e) {
+                        Log.log(Level.SEVERE, e.toString(), e);
+                        System.exit(1);
+                    }
+                }
+            });
         } catch (Exception e) {
-            Log.log(Level.SEVERE, "main: ‘" + e.toString() + "’", e);
-            e.printStackTrace();
+            Log.log(Level.SEVERE, e.toString(), e);
+            System.exit(1);
         }
+
     }
 
     // CALLS FROM STORAGE SERVER
-    public void addStorageServer(String host, String top_dir) throws Exception {
+    public void addStorageServer(String hostname, String top_dir) throws Exception {
         // Example: addStorageServer("machine1.dcc.fc.up.pt", "/courses");
         if (!checkTopDir(top_dir)) {
-            Log.log(Level.SEVERE, "addStorageServer: invalid top directory", top_dir);
+            Log.log(Level.SEVERE, "invalid top directory ‘" + top_dir + "’");
             throw new Exception("invalid top directory ‘" + top_dir + "’");
         }
         if (StorageServers.containsKey(top_dir)) {
-            Log.log(Level.SEVERE, "addStorageServer: top directory already in use", top_dir);
-            throw new Exception("top directory ‘" + top_dir + "’already in use");
+            Log.log(Level.SEVERE, "top directory already in use ‘" + top_dir + "’");
+            throw new Exception("top directory already in use ‘" + top_dir + "’");
         }
-
-        try {
-            FileSystem.addNode(top_dir);
-            StorageServers.put(top_dir, host);
-        } catch (Exception e) {
-            Log.log(Level.SEVERE, "addStorageServer: ‘" + e.toString() + "’", e);
-            throw e;
+        if (!FileSystem.addNode(top_dir)) {
+            Log.log(Level.SEVERE, "no such file or directory ‘" + top_dir + "’");
+            throw new Exception("no such file or directory ‘" + top_dir + "’");
         }
-        Log.log(Level.INFO, "addStorageServer: ‘" + host + "’", top_dir);
+        StorageServers.put(top_dir, hostname);
+        Log.log(Level.INFO, hostname + ", " + top_dir);
     }
 
     public void delStorageServer(String top_dir) throws Exception {
         // Example: delStorageServer("/courses");
         if (!checkTopDir(top_dir)) {
-            Log.log(Level.SEVERE, "delStorageServer: invalid top directory", top_dir);
+            Log.log(Level.SEVERE, "invalid top directory ‘" + top_dir + "’");
             throw new Exception("invalid top directory ‘" + top_dir + "’");
         }
         if (!StorageServers.containsKey(top_dir)) {
-            Log.log(Level.SEVERE, "delStorageServer: top directory not found", top_dir);
-            throw new Exception("top directory ‘" + top_dir + "’not found");
+            Log.log(Level.SEVERE, "top directory not found ‘" + top_dir + "’");
+            throw new Exception("top directory not found ‘" + top_dir + "’");
         }
-
-        try {
-            FileSystem.delNode(top_dir);
-            StorageServers.remove(top_dir);
-        } catch (Exception e) {
-            Log.log(Level.SEVERE, "delStorageServer: ‘" + e.toString() + "’", e);
-            throw e;
+        if (!FileSystem.delNode(top_dir)) {
+            Log.log(Level.SEVERE, "no such file or directory ‘" + top_dir + "’");
+            throw new Exception("no such file or directory ‘" + top_dir + "’");
         }
-        Log.log(Level.INFO, "delStorageServer: ‘" + top_dir + "’");
+        StorageServers.remove(top_dir);
+        Log.log(Level.INFO, top_dir);
     }
 
     public void addStorageItem(String item) throws Exception {
         // Example: addStorageItem("/courses/video1.avi");
+        if (checkTopDir(item)) {
+            Log.log(Level.SEVERE, "can not add item in root directory ‘" + item + "’");
+            throw new Exception("can not add item in root directory ‘" + item + "’");
+        }
         if (!checkPath(item)) {
-            Log.log(Level.SEVERE, "addStorageItem: invalid path", item);
+            Log.log(Level.SEVERE, "invalid path ‘" + item + "’");
             throw new Exception("invalid path ‘" + item + "’");
         }
-
-        try {
-            FileSystem.addNode(item);
-        } catch (Exception e) {
-            Log.log(Level.SEVERE, "addStorageItem: ‘" + e.toString() + "’", e);
-            throw e;
+        if (!FileSystem.addNode(item)) {
+            Log.log(Level.SEVERE, "no such file or directory ‘" + item + "’");
+            throw new Exception("no such file or directory ‘" + item + "’");
         }
-        Log.log(Level.INFO, "addStorageItem: ‘" + item + "’");
+        Log.log(Level.INFO, item);
     }
 
     public void delStorageItem(String item) throws Exception {
         // Example: delStorageItem("/courses/video1.avi");
+        if (checkTopDir(item)) {
+            Log.log(Level.SEVERE, "can not delete item in root directory ‘" + item + "’");
+            throw new Exception("can not delete item in root directory ‘" + item + "’");
+        }
         if (!checkPath(item)) {
-            Log.log(Level.SEVERE, "delStorageItem: invalid path", item);
+            Log.log(Level.SEVERE, "invalid path ‘" + item + "’");
             throw new Exception("invalid path ‘" + item + "’");
         }
-
-        try {
-            FileSystem.delNode(item);
-        } catch (Exception e) {
-            Log.log(Level.SEVERE, "delStorageItem: ‘" + e.toString() + "’", e);
-            throw e;
+        if (!FileSystem.delNode(item)) {
+            Log.log(Level.SEVERE, "no such file or directory ‘" + item + "’");
+            throw new Exception("no such file or directory ‘" + item + "’");
         }
-        Log.log(Level.INFO, "delStorageItem: ‘" + item + "’");
+        Log.log(Level.INFO, item);
     }
 
     // CALLS FROM CLIENT
     public String find(String path) throws Exception {
         // Example: find("/courses"); -> "machine1.dcc.fc.up.pt"
         if (!checkPath(path)) {
-            Log.log(Level.SEVERE, "find: invalid path", path);
+            Log.log(Level.SEVERE, "invalid path ‘" + path + "’");
             throw new Exception("invalid path ‘" + path + "’");
         }
-
-        String top_dir = splitPath(path)[0];
-        String host = StorageServers.get(top_dir);
-        if (host == null) {
-            Log.log(Level.SEVERE, "find: top directory not found", top_dir);
-            throw new Exception("top directory ‘" + top_dir + "’ not found");
+        if (FileSystem.getNode(path) == null) {
+            Log.log(Level.SEVERE, "no such file or directory ‘" + path + "’");
+            throw new Exception("no such file or directory ‘" + path + "’");
         }
-        Log.log(Level.INFO, "find: ‘" + path + "’", host);
-        return host;
+        String top_dir = "/" + splitPath(path)[0];
+        String storage = StorageServers.get(top_dir);
+        if (storage == null) {
+            Log.log(Level.SEVERE, "storage server not found ‘" + path + "’");
+            throw new Exception("storage server not found ‘" + path + "’");
+        }
+        Log.log(Level.INFO, path, storage);
+        return storage;
     }
 
     public Stat lstat(String path) throws Exception {
         // Example: lstat("/courses"); -> { "machine1.dcc.fc.up.pt", { "afile.txt", "bfile.txt", "..." } }
+        if (path.equals("/")) {
+            List<String> storage_hosts = new ArrayList<String>();
+            storage_hosts.addAll(StorageServers.keySet());
+            return new Stat("MetaData", storage_hosts);
+        }
         if (!checkPath(path)) {
-            Log.log(Level.SEVERE, "lstat: invalid path", path);
+            Log.log(Level.SEVERE, "invalid path ‘" + path + "’");
             throw new Exception("invalid path ‘" + path + "’");
         }
-
-        Stat stat = new Stat();
-        try {
-            FSNode target = FileSystem.getNode(path);
-            stat.server = find(path);
-            stat.items = target.getChildsNames();
-        } catch (Exception e) {
-            Log.log(Level.SEVERE, "lstat: ‘" + e.toString() + "’", e);
-            throw e;
+        FSNode target = FileSystem.getNode(path);
+        if (target == null) {
+            Log.log(Level.SEVERE, "no such file or directory ‘" + path + "’");
+            throw new Exception("no such file or directory ‘" + path + "’");
         }
-        Log.log(Level.INFO, "lstat: ‘" + path + "’");
-        return stat;
+        String top_dir = "/" + splitPath(path)[0];
+        String storage = StorageServers.get(top_dir);
+        if (storage == null) {
+            Log.log(Level.SEVERE, "storage server not found ‘" + path + "’");
+            throw new Exception("storage server not found ‘" + path + "’");
+        }
+        Log.log(Level.INFO, path);
+        return new Stat(storage, target.getChildsNames());
     }
 
     private boolean checkTopDir(String top_dir) {
