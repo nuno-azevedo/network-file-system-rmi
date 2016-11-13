5b438599d11961010603577a525592368c54462c
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-11-13 02:42:37 +0000

    New method nano, fix (mkdir, ls, cd), error checking
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/Client.java
+++ src/Client.java
@@ -1,34 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Client {
     private static Registry registry;
     private static MetaDataInterface MetaData;
-    private static HashMap<String, StorageInterface> StorageServers;
-    private static String LocalPath;
     private static String CurrentDir;
+    private static String LocalPath;
 
     private Client() {
-        CurrentDir = "/";
+
     }
 
     public static void main(String args[]) throws RemoteException {
-        if (args.length != 1) {
-            System.err.println("USAGE: java Client $LOCAL_PATH");
+        if (args.length != 2) {
+            System.err.println("USAGE: java Client $LOCAL_PATH $METADATA_HOSTNAME");
             System.exit(1);
         }
 
         try {
             registry = LocateRegistry.getRegistry();
-            MetaData = (MetaDataInterface) registry.lookup("MetaData");
+            MetaData = (MetaDataInterface) registry.lookup(args[1]);
         } catch (Exception e) {
-            System.err.println(e.toString());
+            System.err.println(e.getMessage());
             System.exit(1);
         }
 
+        CurrentDir = "/";
         LocalPath = args[0];
         Scanner scan = new Scanner(System.in);
         String cmd = new String();
@@ -73,9 +76,9 @@ public class Client {
             else if (cmd_list.length == 2) open(cmd_list[1]);
             else System.err.println(cmd_list[0] + ": too many arguments");
         }
-        else if (cmd_list[0].equals("touch")) {
-            if (cmd_list.length < 3) System.err.println(cmd_list[0] + ": missing arguments");
-            else if (cmd_list.length == 3) touch(cmd_list[1], cmd_list[2]);
+        else if (cmd_list[0].equals("nano")) {
+            if (cmd_list.length == 1) System.err.println(cmd_list[0] + ": missing arguments");
+            else if (cmd_list.length == 2) nano(cmd_list[1]);
             else System.err.println(cmd_list[0] + ": too many arguments");
         }
         else System.err.println(cmd_list[0] + ": command not found");
@@ -86,10 +89,17 @@ public class Client {
     }
 
     private static void ls(String dir) {
+        String path = parsePath(dir);
         try {
-            System.out.println(MetaData.lstat(dir).toString());
+            Stat stat = MetaData.lstat(path);
+            java.util.Collections.sort(stat.items);
+            for (int i = 0; i < stat.items.size(); i++) {
+                if (i % 5 == 0 && i != 0) System.out.println();
+                System.out.print(stat.items.get(i) + "\t");
+            }
+            System.out.println();
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println(e.getMessage());
             return;
         }
     }
@@ -97,41 +107,52 @@ public class Client {
     private static void cd(String dir) {
         // Changes the current directory to dir
         // dir can be a simple name or absolute or relative path
+        String path = parsePath(dir);
         try {
-            MetaData.find(dir);
+            MetaData.find(path);
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println(e.getMessage());
             return;
         }
-        CurrentDir = dir;
+        CurrentDir = path;
     }
 
     private static void mkdir(String dir) {
+        String path = parsePath(dir);
+        if (checkTopPath(path)) {
+            System.out.println("cannot create directory ‘" + dir + "’: not allowed on root directory");
+            return;
+        }
         try {
-            String server = MetaData.find(dir);
-            StorageInterface stub = StorageServers.get(server);
-            if (stub == null) {
-                stub = (StorageInterface) registry.lookup(server);
-                StorageServers.put(server, stub);
-            }
-            stub.create(dir);
+            String top_dir = getTopPath(path);
+            String server = MetaData.find(top_dir);
+            StorageInterface stub = (StorageInterface) registry.lookup(server);
+            stub.create(path);
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println(e.getMessage());
             return;
         }
     }
 
-    private static void touch(String file, String blob) {
+    private static void nano(String file) {
+        String path = parsePath(file);
+        if (checkTopPath(path)) {
+            System.out.println("cannot create file ‘" + file + "’: not allowed on root directory");
+            return;
+        }
         try {
-            String server = MetaData.find(file);
-            StorageInterface stub = StorageServers.get(server);
-            if (stub == null) {
-                stub = (StorageInterface) registry.lookup(server);
-                StorageServers.put(server, stub);
+            String top_dir = getTopPath(path);
+            String server = MetaData.find(top_dir);
+            StorageInterface stub = (StorageInterface) registry.lookup(server);
+            BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+            String line, blob = new String();
+            while((line = systemIn.readLine()) != null) {
+                blob = blob.concat(line).concat("\n");
             }
-            stub.create(file, blob);
+            stub.create(path, blob);
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+            return;
         }
     }
 
@@ -141,15 +162,44 @@ public class Client {
     }
 
     private static void open(String file) {
+        System.out.println(parsePath(file));
+        if (LocalPath != null);
         // Opens the file with the proper application, accordingly to its extension
         // file can be a simple name or absolute or relative path
     }
 
     private static String parsePath(String path) {
-        return path;
+        if (!path.startsWith("/")) {
+            if (CurrentDir.endsWith("/")) path = CurrentDir + path;
+            else path = CurrentDir + "/" + path;
+        }
+        List<String> parsed = new ArrayList<String>();
+        String nodes[] = splitPath(path);
+        for (String node : nodes) {
+            if (node.equals("/."));
+            else if (node.equals("/..")) parsed.remove(parsed.size() - 1);
+            else parsed.add(node);
+        }
+        return String.join("", parsed);
+    }
+
+    private static boolean checkAbsPath(String path) {
+        String valid_path = "^/((?!/\\.{2,}(/|$)|//).)*(?<!/)$";
+        if (path.matches(valid_path)) return true;
+        return false;
+    }
+
+    private static boolean checkTopPath(String top_dir) {
+        String valid_top_dir = "^/((?!/\\.{2,}(/|$)|//|/).)*$";
+        if (top_dir.matches(valid_top_dir)) return true;
+        return false;
+    }
+
+    private static String getTopPath(String path) {
+        return path.split("(?=/)")[0];
     }
 
     private static String[] splitPath(String path) {
-        return path.replaceAll("^/", "").replaceAll("/$", "").split("/");
+        return path.split("(?=/)");
     }
 }
