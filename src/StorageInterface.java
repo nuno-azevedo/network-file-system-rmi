c30bc2aa22313f2a799aaf64618baddd3d8692fc
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-11-14 19:09:51 +0000

    Change argument names
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/StorageInterface.java
+++ src/StorageInterface.java
@@ -1,13 +1,12 @@
-import java.io.File;
 import java.rmi.Remote;
 
 public interface StorageInterface extends Remote {
     // CALLS FROM CLIENT
-    void create(String path) throws Exception;
+    void create(String item) throws Exception;
 
-    void create(String path, String blob) throws Exception;
+    void create(String item, byte bytes[]) throws Exception;
 
-    void del(String path) throws Exception;
+    void del(String item) throws Exception;
 
-    File get(String path) throws Exception;
+    byte[] get(String item) throws Exception;
 }
