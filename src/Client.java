a39a9d5de3f100f94ead9e0829acb05f9857f8ed
Nuno Azevedo <nuno.azevedo@movvo.com>
2016-11-14 01:55:02 +0000

    Remove unused import
    Signed-off-by: Nuno Azevedo <nuno.azevedo@movvo.com>

--- src/Client.java
+++ src/Client.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
 
 public class Client {
     private static Registry registry;
